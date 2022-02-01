4a05a0fbb93785baafcb3f86139f1acac53bbc83
Kristian Hansen <kristian.hansen.no@hotmail.no>
2022-02-01 16:58:19 +0100

    oppgave2

--- oving-2/knut-ola/oppgave-2/a.ino
+++ oving-2/knut-ola/oppgave-2/a.ino
@@ -1,7 +1,44 @@
+//lager variabler så det er enklere å endre koden
+const int sensorPin = A0;
+//endrer disse til float, så jeg får R (hvis ikke 0)
+int sensorValue;
+int sensorValueVoltage;
+int sensorValueVoltage2;
+int lastSensorValueVoltage;
+int i = 0;
+
+const char *meld[5] = {"1V, data: ", "2V, data: ", "3V, data: ",
+                       "4V, data: ", "5V, data: "};
+
 void setup()
 {
+    Serial.begin(9600);
 }
 
 void loop()
 {
+    sensorValue = analogRead(sensorPin);
+
+    sensorValueVoltage = sensorValue / 200;
+    sensorValueVoltage2 = sensorValue;
+
+    if (i == 0)
+    { // forsikrer at det blir printet noe første gang
+        Serial.println(meld[sensorValueVoltage]);
+        Serial.println(sensorValueVoltage2);
+        i++;
+    }
+
+    // printer bare hvis verdien endrer seg
+    if (sensorValueVoltage != lastSensorValueVoltage)
+    {
+
+        Serial.println(meld[sensorValueVoltage]);
+        Serial.println(sensorValueVoltage2);
+    }
+
+    // lagrer den forje verdien, så vi kan se om det er endring
+    lastSensorValueVoltage = sensorValueVoltage;
+
+    //delay(500);
 }
