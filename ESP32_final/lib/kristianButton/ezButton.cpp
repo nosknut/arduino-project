797162adb692a6b5816648ab07dfb50558eb2900
Kristian Hansen <kristian.hansen.no@hotmail.no>
2022-03-10 13:13:17 +0100

    lagt til kristianButton i lib

---
+++ ESP32_final/lib/kristianButton/ezButton.cpp
@@ -0,0 +1,69 @@
+#include <kristianButton.h>
+
+// husk å legg til "pinMode(BUTTON_PIN, INPUT_PULLUP);"
+kristianButton::kristianButton(int pin)
+{
+	int btnPin = pin;
+	int debounceTime = 0;
+	int lastSteadyState = LOW;		// the previous steady state from the input pin
+	int lastFlickerableState = LOW; // the previous flickerable state from the input pin
+	int currentState;				// the current reading from the input pin
+	// denne endres med setLoop funksjon
+	bool theButtonState = false;
+
+	// the following variables are unsigned longs because the time, measured in
+	// milliseconds, will quickly become a bigger number than can be stored in an int.
+	unsigned long lastDebounceTime = 0; // the last time the output pin was toggled
+}
+
+bool kristianButton::buttonState(void)
+{
+	if (theButtonState == true)
+	{
+		return true;
+	}
+	else if (theButtonState == false)
+	{
+		return false;
+	}
+}
+
+void kristianButton::debounce(int time)
+{
+	debounceTime = time;
+}
+
+void kristianButton::setLoop(void)
+{
+	// leser av status på knapp (denne vil også avlese støy)
+	currentState = digitalRead(btnPin);
+
+	// denne sjekker hele tiden om det er støy
+	// altså om current state har endret seg raskere enn debounce
+	if (currentState != lastFlickerableState)
+	{
+		// hvis "støy" -> reset debounce
+		lastDebounceTime = millis();
+		// lagrer dette resultatet for å sjekke neste gang
+		lastFlickerableState = currentState;
+	}
+
+	if ((millis() - lastDebounceTime) > debounceTime)
+	{
+		// hvis du kommer inn i denne funksjonen, så har avlesningen vært på
+		// lenger enn debounce tid, ergo:
+
+		// hvis currentstate har endret seg:
+		if (lastSteadyState == HIGH && currentState == LOW)
+		{
+			theButtonState = true;
+		}
+		else if (lastSteadyState == LOW && currentState == HIGH)
+		{
+			theButtonState = false;
+		}
+
+		// lagrer lastSteadyState
+		lastSteadyState = currentState;
+	}
+}
--- ESP32_final/src/main.cpp
+++ ESP32_final/src/main.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
-#include <ezButton.h>
+#include <kristianButton.h>
 // lagt til lib_deps = ezButton... tror d er riktig
 
-// inneholder A til B
+// inneholder A til B + TEST
 enum class OppgaveStatus
 {
   OPPGAVE2_A,
@@ -12,22 +12,14 @@ enum class OppgaveStatus
   TEST
 };
 
-//-----VELG HVILKEN OPPGVAVE SOM SKAL KJØRES------
 OppgaveStatus oppgave = OppgaveStatus::TEST;
 
 // ledcSetup(PWM - kanal(0 - 15), PWM.frekvens, PWM - oppløsning(i bit));
 
-// ezButton buttonLED = 33;
 #define BUTTON_PIN 33    // port fra knapp til ESP32
 #define DEBOUNCE_TIME 50 // debounce tid (millisekunder)
+kristianButton buttonOne = BUTTON_PIN;
 // Variables will change:
-int lastSteadyState = LOW;      // the previous steady state from the input pin
-int lastFlickerableState = LOW; // the previous flickerable state from the input pin
-int currentState;               // the current reading from the input pin
-
-// the following variables are unsigned longs because the time, measured in
-// milliseconds, will quickly become a bigger number than can be stored in an int.
-unsigned long lastDebounceTime = 0; // the last time the output pin was toggled
 
 const int LED = 32;
 
@@ -39,6 +31,7 @@ void setup()
   Serial.begin(9600);
   pinMode(LED, OUTPUT);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
+  buttonOne.debounce(50);
 
   // ESP32 PWM
   ledcSetup(channelLED, 2000, 8);
@@ -61,41 +54,7 @@ void loop()
   }
   if (oppgave == OppgaveStatus::TEST)
   {
-    // read the state of the switch/button:
-    currentState = digitalRead(BUTTON_PIN);
-
-    // check to see if you just pressed the button
-    // (i.e. the input went from LOW to HIGH), and you've waited long enough
-    // since the last press to ignore any noise:
-
-    // If the switch/button changed, due to noise or pressing:
-    if (currentState != lastFlickerableState)
-    {
-      // reset the debouncing timer
-      lastDebounceTime = millis();
-      // save the the last flickerable state
-      lastFlickerableState = currentState;
-    }
-
-    if ((millis() - lastDebounceTime) > DEBOUNCE_TIME)
-    {
-      // whatever the reading is at, it's been there for longer than the debounce
-      // delay, so take it as the actual current state:
-
-      // if the button state has changed:
-      if (lastSteadyState == HIGH && currentState == LOW)
-      {
-        Serial.println("The button is pressed");
-        ledcWrite(channelLED, 255);
-      }
-      else if (lastSteadyState == LOW && currentState == HIGH)
-      {
-        Serial.println("The button is released");
-        ledcWrite(channelLED, 0);
-      }
-
-      // save the the last steady state
-      lastSteadyState = currentState;
-    }
+    buttonOne.setLoop();
+    Serial.println(buttonOne.buttonState());
   }
 }
