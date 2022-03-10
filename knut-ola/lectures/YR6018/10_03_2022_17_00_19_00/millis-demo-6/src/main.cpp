e1d309a47d128f45ef13fb5e52be06af8f8d9dd0
nosknut <nosknut@gmail.com>
2022-03-10 17:19:31 +0100

    adds 6

--- knut-ola/lectures/YR6018/10_03_2022_17_00_19_00/millis-demo-6/src/main.cpp
+++ knut-ola/lectures/YR6018/10_03_2022_17_00_19_00/millis-demo-6/src/main.cpp
@@ -1,8 +1,11 @@
 #include <Arduino.h>
 
+int knappPin = 2;
+
 void setup()
 {
     Serial.begin(9600);
+    pinMode(knappPin, INPUT);
 }
 
 void print(String message)
@@ -10,6 +13,55 @@ void print(String message)
     Serial.println(message);
 }
 
+bool knappVarTryktNedForigeGang = false;
+bool viHarReagertPaAtKnappenBleHoldtNede = false;
+
+bool hondterKnappetrykk()
+{
+    bool knappErTryktNed = digitalRead(knappPin);
+
+    if (knappErTryktNed && !knappVarTryktNedForigeGang)
+    {
+        viHarReagertPaAtKnappenBleHoldtNede = false;
+    }
+
+    if (knappErTryktNed)
+    {
+        if (!viHarReagertPaAtKnappenBleHoldtNede)
+        {
+            viHarReagertPaAtKnappenBleHoldtNede = true;
+            knappVarTryktNedForigeGang = knappErTryktNed;
+            return true;
+        }
+    }
+
+    knappVarTryktNedForigeGang = knappErTryktNed;
+    return false;
+}
+
+int gangerTrykket = 0;
+
 void loop()
 {
+    if (hondterKnappetrykk())
+    {
+        gangerTrykket++;
+        switch (gangerTrykket)
+        {
+        case 1:
+            print("Du har trykket 1 gang");
+            break;
+        case 2:
+            print("Du har trykket 2 ganger");
+            break;
+        case 3:
+            print("Du har trykket 3 ganger");
+            print("Vi starter pa nytt");
+            gangerTrykket = 0;
+            break;
+        default:
+            gangerTrykket = 0;
+            break;
+        }
+    }
 }
