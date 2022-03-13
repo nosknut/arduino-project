652e5337f77c9d83cf5c815e878c5a21f6d7a8b3
nosknut <nosknut@gmail.com>
2022-03-13 12:55:42 +0100

    Adds range warning for PI regulators

--- lab4/knut-ola/lib/PidController/PidController.h
+++ lab4/knut-ola/lib/PidController/PidController.h
@@ -23,6 +23,22 @@ public:
     {
     }
 
+    /**
+     * NB!
+     * For I, PI, ID og PID (anything with I) regulators, the input value MUST be allowed
+     * to exceed the input range. For example, if the input range is [0, 100], the "value" argument
+     * must be allowed to be greater than 100, and less than 0. If this is not done, the regulator
+     * will be unable to calculate the correct output, if the target value is ever set to 100 or 0.
+     * It will also get less responsive as the target value approaches the limits of the inputRange.
+     * In short: Do not restrict the input value to the input range, unless you want
+     * your watertank to overflow.
+     *
+     * @param value the sensor reading
+     * @param target the target sensor reading
+     * @param clampOutput true if the output should always stay
+     * within the output range specified in the config
+     * @return the output value
+     */
     double update(double value, double target, bool clampOutput)
     {
         double error = value - target;
