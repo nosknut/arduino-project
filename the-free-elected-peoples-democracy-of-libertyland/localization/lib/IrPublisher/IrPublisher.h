60cd807c30db57d0444ed105516a169242790e19
nosknut <nosknut@gmail.com>
2022-04-22 15:44:28 +0200

    IrPublisher compiles!

--- the-free-elected-peoples-democracy-of-libertyland/localization/lib/IrPublisher/IrPublisher.h
+++ the-free-elected-peoples-democracy-of-libertyland/localization/lib/IrPublisher/IrPublisher.h
@@ -2,48 +2,42 @@
 #define IrPublisher_h
 #include <Arduino.h>
 #include <ros.h>
-#include <std_msgs/String.h>
+#include <ros/time.h>
+#include <sensor_msgs/Range.h>
 #include <Zumo32U4ProximitySensors.h>
-#include <ros.h>;
-#include <ros/time.h>;
-#include <sensor_msgs/Range.h>;
 
 /*
+ * http://wiki.ros.org/rosserial_arduino/Tutorials/IR%20Ranger
  * rosserial IR Ranger Example
  *
  * This example is calibrated for the Sharp GP2D120XJ00F.
  */
+
 class IrPublisher
 {
 private:
-    ros::NodeHandle nh;
     sensor_msgs::Range range_msg;
-    ros::Publisher pub_range("range_data", &range_msg);
+    ros::Publisher pub_range = ros::Publisher("range_data", &range_msg);
 
-    unsigned long range_timer;
+    String frameid = "/ir_ranger";
 
     Zumo32U4ProximitySensors proximity;
 
+    unsigned long range_timer;
+
 public:
-    IrPublisher()
+    void setup(ros::NodeHandle &nh)
     {
-    }
-
-    char frameid[] = "/ir_ranger";
-
-    void setup()
-    {
-        nh.initNode();
         nh.advertise(pub_range);
 
         range_msg.radiation_type = sensor_msgs::Range::INFRARED;
-        range_msg.header.frame_id = frameid;
+        range_msg.header.frame_id = frameid.c_str();
         range_msg.field_of_view = 0.01;
         range_msg.min_range = 0.03; // For GP2D120XJ00F only. Adjust for other IR rangers
         range_msg.max_range = 0.4;  // For GP2D120XJ00F only. Adjust for other IR rangers
     }
 
-    void loop()
+    void loop(ros::NodeHandle &nh)
     {
         proximity.read();
         // publish the range value every 50 milliseconds
@@ -55,7 +49,6 @@ public:
             pub_range.publish(&range_msg);
             range_timer = millis();
         }
-        nh.spinOnce();
     }
 };
 
--- the-free-elected-peoples-democracy-of-libertyland/localization/src/main.cpp
+++ the-free-elected-peoples-democracy-of-libertyland/localization/src/main.cpp
@@ -1,1 +1,18 @@
 #include <Arduino.h>
+#include <IrPublisher.h>
+
+ros::NodeHandle nh;
+IrPublisher irPublisher;
+
+void setup()
+{
+    nh.initNode();
+    irPublisher.setup(nh);
+}
+
+void loop()
+{
+    irPublisher.loop(nh);
+
+    nh.spinOnce();
+}
