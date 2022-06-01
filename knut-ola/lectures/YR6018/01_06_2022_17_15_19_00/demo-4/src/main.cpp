1acd48e6c56ca4f7123083985309883869509eae
nosknut <nosknut@gmail.com>
2022-06-01 17:15:34 +0200

    Finishes demo-4

--- knut-ola/lectures/YR6018/01_06_2022_17_15_19_00/demo-4/src/main.cpp
+++ knut-ola/lectures/YR6018/01_06_2022_17_15_19_00/demo-4/src/main.cpp
@@ -1,13 +1,81 @@
+// https://randomnerdtutorials.com/esp32-mqtt-publish-subscribe-arduino-ide/
 #include <WiFi.h>
 #include <Arduino.h>
 #include <ArduinoJson.h>
 #include <PubSubClient.h>
 
+class Timer
+{
+private:
+    unsigned long startTime;
+
+public:
+    Timer()
+    {
+        restart();
+    }
+
+    void restart()
+    {
+        startTime = millis();
+    }
+
+    unsigned long getElapsedTime()
+    {
+        return millis() - startTime;
+    }
+
+    bool isFinished(unsigned long duration)
+    {
+        return getElapsedTime() >= duration;
+    }
+
+    bool loopWait(unsigned long duration)
+    {
+        if (isFinished(duration))
+        {
+            restart();
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
+
+class SoftwareBattery
+{
+private:
+    int maxCapacity = 100;
+    int capacity = maxCapacity;
+
+public:
+    void reset()
+    {
+        capacity = maxCapacity;
+    }
+
+    int getCapacity()
+    {
+        if (capacity > 0)
+        {
+            capacity -= 1;
+        }
+        else
+        {
+            capacity = maxCapacity;
+        }
+
+        return capacity;
+    }
+};
+
 String ssid = "Telenor1959bak";
 String password = "yzefbjbqzgxqu";
 
 // https://mntolia.com/10-free-public-private-mqtt-brokers-for-testing-prototyping/
-const String mqtt_server = "mqtt.eclipse.org";
+const String mqtt_server = "test.mosquitto.org";
 const int mqtt_port = 1883;
 
 WiFiClient espClient;
@@ -15,11 +83,16 @@ PubSubClient client(espClient);
 
 String LED_TOPIC = "esp32/led";
 String BATTERY_TOPIC = "esp32/battery";
+String BATTERY_CAPACITY_TOPIC = "esp32/battery/capacity";
 
 String topics[] = {
     LED_TOPIC,
     BATTERY_TOPIC,
+    BATTERY_CAPACITY_TOPIC,
 };
+
+Timer timer;
+SoftwareBattery battery;
 
 void ensureWifiIsConnected()
 {
@@ -48,30 +121,34 @@ void subscribeToTopics()
     for (String topic : topics)
     {
         client.subscribe(topic.c_str());
+        Serial.println("Subscribed to " + topic);
     }
 }
 
 void ensureMqttClientIsConnected()
 {
-    // Loop until we're reconnected
-    while (!client.connected())
+    if (!client.connected())
     {
-        Serial.print("Attempting MQTT connection...");
-        // Attempt to connect
-        if (client.connect("ESP8266Client"))
+        // Loop until we're reconnected
+        while (!client.connected())
         {
-            Serial.println("connected");
+            Serial.print("Attempting MQTT connection...");
+            // Attempt to connect
+            if (client.connect(""))
+            {
+                Serial.println("connected");
 
-            // Subscribe
-            subscribeToTopics();
-        }
-        else
-        {
-            Serial.print("failed, rc=");
-            Serial.print(client.state());
-            Serial.println(" try again in 5 seconds");
-            // Wait 5 seconds before retrying
-            delay(5000);
+                // Subscribe
+                subscribeToTopics();
+            }
+            else
+            {
+                Serial.print("failed, rc=");
+                Serial.print(client.state());
+                Serial.println(" try again in 5 seconds");
+                // Wait 5 seconds before retrying
+                delay(5000);
+            }
         }
     }
 }
@@ -82,7 +159,8 @@ void onLedMessage(String message)
     {
         Serial.println("Turning led on");
     }
-    else if (message == "off")
+
+    if (message == "off")
     {
         Serial.println("Turning led off");
     }
@@ -93,13 +171,24 @@ void onBatteryMessage(String message)
     if (message == "reset")
     {
         Serial.println("Resetting battery");
+        battery.reset();
     }
+}
+
+String stringFromByteArray(byte *message, unsigned int length)
+{
+    String result;
+    for (int i = 0; i < length; i++)
+    {
+        result += (char)message[i];
+    }
+    return result;
 }
 
 void callback(char *topic, byte *message, unsigned int length)
 {
     String topicString = String(topic);
-    String messageString = String((char *)message);
+    String messageString = stringFromByteArray(message, length);
 
     if (topicString == LED_TOPIC)
     {
@@ -124,6 +213,16 @@ void setup()
 void loop()
 {
     ensureWifiIsConnected();
+    ensureMqttClientIsConnected();
+    client.loop();
+
+    if (timer.loopWait(3000))
+    {
+        int batteryCapacity = battery.getCapacity();
+        String batteryCapacityString = String(batteryCapacity);
+
+        client.publish(BATTERY_CAPACITY_TOPIC.c_str(), batteryCapacityString.c_str());
+    }
 
     if (Serial.available())
     {
